4adb40c659ca346a73767ec7f82cbd0a30771ebf
light <leiteng@gmail.com>
2025-09-12 08:35:07 +0800

    Add pseudo latitude/longitude grid lines to Frame Renderer visualization

--- src/demo/visualization/frame_renderer.py
+++ src/demo/visualization/frame_renderer.py
@@ -85,6 +85,31 @@ def render_frames(context: PluginContext) -> None:
             relative_y = (world_y - cam_world_y) * config.zoom_factor
             return (relative_x + viewport_width / 2, relative_y + viewport_height / 2)
 
+        # --- Draw Pseudo Grid Lines (Latitude/Longitude) ---
+        # Draw grid lines every 10 world units
+        grid_spacing_world = 10.0
+        grid_color = (50, 50, 50)  # Dark gray
+        
+        # Calculate the range of grid lines to draw based on zoom factor and viewport size
+        half_width_world = (viewport_width / 2) / config.zoom_factor
+        half_height_world = (viewport_height / 2) / config.zoom_factor
+        
+        # Determine the range of grid lines to draw
+        min_grid_x = int((cam_world_x - half_width_world) / grid_spacing_world) * grid_spacing_world
+        max_grid_x = int((cam_world_x + half_width_world) / grid_spacing_world) * grid_spacing_world
+        min_grid_y = int((cam_world_y - half_height_world) / grid_spacing_world) * grid_spacing_world
+        max_grid_y = int((cam_world_y + half_height_world) / grid_spacing_world) * grid_spacing_world
+        
+        # Draw vertical grid lines (longitude)
+        for x in np.arange(min_grid_x, max_grid_x + grid_spacing_world, grid_spacing_world):
+            x_cam, _ = world_to_camera(x, cam_world_y)
+            draw.line([(x_cam, 0), (x_cam, viewport_height)], fill=grid_color, width=1)
+        
+        # Draw horizontal grid lines (latitude)
+        for y in np.arange(min_grid_y, max_grid_y + grid_spacing_world, grid_spacing_world):
+            _, y_cam = world_to_camera(cam_world_x, y)
+            draw.line([(0, y_cam), (viewport_width, y_cam)], fill=grid_color, width=1)
+
         # --- Draw Grid and Data Points ---
         radius = config.circle_radius_px
         width = config.circle_width_px
